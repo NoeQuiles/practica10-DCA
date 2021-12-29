2e5bc8de4910acdc80f0a5ac8595143aa78a7146
Noelia Quiles <nqg4@alu.ua.es>
2021-12-29 20:03:25 +0100

    5º commit

--- calculadora.c
+++ calculadora.c
@@ -12,8 +12,7 @@ float potencia(float base, int exponente) {
 
 int main() {
     float x, y, resultado;
-    char op;
-    char option;
+    char op
     int valido = 1;
 
     printf("Ingrese operacion: ");
